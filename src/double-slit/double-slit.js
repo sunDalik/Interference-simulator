e2c336fd5e2d8c24b0dcd19483afa9ce8e2dbb73
sunDalik <dali1999@mail.ru>
2019-06-19 15:56:41 +0300

    the plot elements looks nice

--- src/double-slit/double-slit.js
+++ src/double-slit/double-slit.js
@@ -26,7 +26,7 @@ function makeDraggableHorizontally(element) {
             (element.id === "light-source" && newPositionR < document.getElementById("two-slits").getBoundingClientRect().left ||
                 element.id === "two-slits" && newPositionR < document.getElementById("screen").getBoundingClientRect().left && newPositionL > document.getElementById("light-source").getBoundingClientRect().right ||
                 element.id === "screen" && newPositionL > document.getElementById("two-slits").getBoundingClientRect().right ||
-                element.id === "interference-pattern")) {
+                element.id === "interference-pattern" || element.id === "interference-plot")) {
             element.style.left = newPositionL + "px";
         }
     }
@@ -38,17 +38,17 @@ function makeDraggableHorizontally(element) {
 
 function calculateInterferencePlot() {
     let svg = document.getElementById('interference-plot');
-    let amplitude = 50;
+    let amplitude = 100;
     let rarity = 1; // point spacing
     let freq = 0.1; // angular frequency
     let T = 1; // interference period
 
     for (let i = 0; i < 100; i++) {
         const line = document.createElementNS("http://www.w3.org/2000/svg", "line");
-        line.setAttribute('x1', i * rarity);
-        line.setAttribute('y1', Math.cos(freq * Math.PI * i / T) ** 2 * amplitude);
-        line.setAttribute('x2', (i + 1) * rarity);
-        line.setAttribute('y2', Math.cos(freq * Math.PI * (i + 1) / T) ** 2 * amplitude);
+        line.setAttribute('y1', i * rarity);
+        line.setAttribute('x1', Math.cos(freq * Math.PI * i / T) ** 2 * amplitude);
+        line.setAttribute('y2', (i + 1) * rarity);
+        line.setAttribute('x2', Math.cos(freq * Math.PI * (i + 1) / T) ** 2 * amplitude);
         line.setAttribute('stroke', 'black');
         line.setAttribute('stroke-width', '1'); //might change later
 
